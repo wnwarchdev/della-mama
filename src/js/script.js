89df9331978814b2187af65a12169f986b06e723
Wojtek Nowak <billnewmann@gmail.com>
2020-02-06 17:49:06 +0000

    Add cart menu

--- src/js/script.js
+++ src/js/script.js
@@ -320,12 +320,33 @@
       const event = new Event ('custom');
       thisWidget.element.dispatchEvent(event); //dispatch event?
       //alert('happened');
+    }
+  } // end AmountWidget
 
+
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+      thisCart.products=[];
+      thisCart.getElements(element);
+      thisCart.initActions();
+      //console.log('new Cart', thisCart);
+    }
+    getElements(element){
+      const thisCart = this;
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function () {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
 
     }
-
-
-  } // end AmountWidget
+  }
 
   const app = {
     initMenu: function(){
@@ -342,6 +363,12 @@
       thisApp.data = dataSource;
     },
 
+    initCart: function(){
+      const thisApp = this;
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart (cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       //console.log('*** App starting ***');
@@ -353,6 +380,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
